a45dfb63641130c5b3cece856ca724173656704f
MUY SENGLY <muysengly@gmail.com>
2025-02-17 14:35:12 +0700

    upload

---
+++ dir_002_listing_code/chapter_001/Welcome.java
@@ -0,0 +1,10 @@
+
+public class Welcome {
+    public static void main(String[] args) {
+        // Display message Welcome to Java! on the console
+        System.out.println("Welcome to Java!");
+    }
+}
+
+// What is public? Answer: public is an access modifier. It is a keyword that is
+// used to specify the visibility of a class, method, or field.
