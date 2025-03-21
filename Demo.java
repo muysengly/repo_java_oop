f66ceeac45ba8ede0d6fc67dff7eb654af38f83a
MUY SENGLY <muysengly@gmail.com>
2025-03-21 11:15:09 +0700

    update

--- Demo.java
+++ Demo.java
@@ -2,12 +2,18 @@
 public class Demo {
     public static void main(String[] args) {
 
-        for (int i = 10; i > 0; i--) {
-            System.out.println(i);
-            try {
-                Thread.sleep(1000); // Pause for 1 second
-            } catch (InterruptedException e) {
+        System.out.println("Multiplication Table");
+        System.out.print("   ");
+        for (int j = 1; j <= 9; j++) {
+            System.out.print("   " + j);
+        }
+        System.out.println("\n-----------------------------");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + " | ");
+            for (int j = 1; j <= 9; j++) {
+                System.out.printf("%4d", i * j);
             }
+            System.out.println();
         }
 
     }
---
+++ demo_005/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Scene scene = FXMLLoader.load(getClass().getResource("View.fxml"));
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
+}
