d499186faa74a658951f2f87a80c4bba93776803
MUY SENGLY <muysengly@gmail.com>
2025-03-30 02:17:12 +0700

    update

--- Demo.java
+++ Demo.java
@@ -1,20 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class Demo {
     public static void main(String[] args) {
-
-        System.out.println("Multiplication Table");
-        System.out.print("   ");
-        for (int j = 1; j <= 9; j++) {
-            System.out.print("   " + j);
-        }
-        System.out.println("\n-----------------------------");
-        for (int i = 1; i <= 9; i++) {
-            System.out.print(i + " | ");
-            for (int j = 1; j <= 9; j++) {
-                System.out.printf("%4d", i * j);
-            }
-            System.out.println();
-        }
-
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss")));
     }
+
 }
--- dir_005_mini_project/mini_005/Main.java
+++ dir_005_mini_project/mini_005/Main.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,11 +11,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
         Scene scene = FXMLLoader.load(getClass().getResource("View.fxml"));
-
         stage.setScene(scene);
-
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("itc_logo.png")));
+        stage.setTitle("Mini Project");
         stage.show();
     }
 }
