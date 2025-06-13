2a06a7672551cb43f82116c446714b41be707391
MUY SENGLY <muysengly@gmail.com>
2025-06-14 01:31:09 +0700

    update

--- TIP.java
+++ TIP.java
@@ -1,31 +1,27 @@
-class Animal {
-    String name;
-
-    Animal(String name) {
-        this.name = name;
-    }
-
-    void printName() {
-        System.out.println("Animal name: " + name);
-    }
-}
-
-class Dog extends Animal {
-    Dog() {
-        super("");
-    } // Provide default constructor
-
-    @Override
-    void printName() {
-        super.printName();
-        System.out.println("Dog name: " + name);
-    }
-}
+import java.util.ArrayList;
 
 public class TIP {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.name = "Buddy";
-        dog.printName();
+        // Create an ArrayList of Strings
+        ArrayList<String> fruits = new ArrayList<>();
+
+        // Add elements to the ArrayList
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+
+        // Access elements
+        System.out.println("First fruit: " + fruits.get(0));
+
+        // Iterate over the ArrayList
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        // Remove an element
+        fruits.remove("Banana");
+
+        // Print the ArrayList after removal
+        System.out.println("After removal: " + fruits);
     }
 }
